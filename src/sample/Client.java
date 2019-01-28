318665dac1945467607792b8f3b5311cf2d2cab2
SenamDom <Senam.dom@jensim.fr>
2019-01-28 19:29:21 +0100

    client

--- src/sample/Client.java
+++ src/sample/Client.java
@@ -1,16 +1,24 @@
 package sample;
 
+import javafx.application.Application;
+import javafx.stage.Stage;
+
 import java.net.MalformedURLException;
 import java.rmi.Naming;
 import java.rmi.NotBoundException;
 import java.rmi.RemoteException;
 
-public class Client {
+public class Client extends Application {
 
     public static void main(String argv[]) throws RemoteException, MalformedURLException, NotBoundException {
-        ZooImpl monZoo = (ZooImpl)Naming.lookup("//localhost/ZooImpl");
-        monZoo.beginZoo(); // PArametre Stage ??
-        monZoo.toString(); // ajouter un toString au Zoo ?
+        launch(argv);
 
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws RemoteException, MalformedURLException,Exception {
+
+        ZooImpl monZoo = (ZooImpl)Naming.lookup("//localhost/ZooImpl");
+        monZoo.beginZoo(primaryStage);
     }
 }
