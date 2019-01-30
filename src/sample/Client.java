0e7c5e8c1c3cf95734d391e221b495329aa46bfa
Senam <afua.dom.etu@uni-lemans.fr>
2019-01-30 15:31:00 +0100

    Client+
    serveur

--- src/sample/Client.java
+++ src/sample/Client.java
@@ -3,22 +3,29 @@ package sample;
 import javafx.application.Application;
 import javafx.stage.Stage;
 
-import java.net.MalformedURLException;
+//import java.net.MalformedURLException;
 import java.rmi.Naming;
 import java.rmi.NotBoundException;
 import java.rmi.RemoteException;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
 
-public class Client extends Application {
+public class Client{
 
-    public static void main(String argv[]) throws RemoteException, MalformedURLException, NotBoundException {
-        launch(argv);
+    public static void main(String argv[]) throws RemoteException, NotBoundException {
+        Registry a= LocateRegistry.getRegistry("localhost",1099);
+        Zoo zoo = (Zoo) a.lookup("zoo");
+        //launch(argv);
 
     }
 
-    @Override
-    public void start(Stage primaryStage) throws RemoteException, MalformedURLException,Exception {
+    /*@Override
+    public void start(Stage primaryStage) throws RemoteException,Exception {
 
-        ZooImpl monZoo = (ZooImpl)Naming.lookup("//localhost/ZooImpl");
-        monZoo.beginZoo(primaryStage);
-    }
+
+        //ZooImpl monZoo = new ZooImpl();
+        //a.bind("Zoo",monZoo);
+        //System.out.println("Zoo: "+zoo);
+        //monZoo.beginZoo(primaryStage);
+}*/
 }
--- src/sample/Serveur.java
+++ src/sample/Serveur.java
@@ -1,13 +1,19 @@
 package sample;
 
 import java.rmi.Naming;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
 
 public class Serveur  {
 
     public static void main(String[] args) {
+
         try {
             ZooImpl monZoo = new ZooImpl();
-            Naming.bind("rmi://localhost/ZooImpl", monZoo);
+            Registry a= LocateRegistry.createRegistry(1089);
+            //ZooImpl monZoo = (ZooImpl)Naming.lookup("//localhost/ZooImpl");
+            a.bind("zoo",monZoo);
+           // Naming.bind("rmi://localhost/ZooImpl", monZoo);
             System.out.println("Zoo enregistré " + monZoo);
         } catch (Exception e) {
             System.out.println("ERREUR");
